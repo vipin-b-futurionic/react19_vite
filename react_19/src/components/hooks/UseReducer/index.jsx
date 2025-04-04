cfa6c2f0fd33f85b40a529ee366ae8c6584bf41e
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-04 16:37:48 +0530

    use reducer fun

--- react_19/src/components/hooks/UseReducer/index.jsx
+++ react_19/src/components/hooks/UseReducer/index.jsx
@@ -1,26 +1,50 @@
-import { useReducer, } from "react"
+import { useReducer } from "react";
 
 export const ReducerComp = () => {
+  const initialState = {
+    count: 0,
+    inc: 2,
+    dec: 2,
+  };
 
+  const reducer = (state, action) => {
+    console.log(state, action);
+    // if (action.type === "INCREMENT") {
+    //   return state + 1;
+    // }
+    // if (action.type === "DECREMENT") {
+    //   return state - 1;
+    // }
+    // if (action.type === "RESET") {
+    //   return (state = 0);
+    // }
 
-    const reducer = (state, action) => {
-        console.log(state, action)
-        if(action.type === "INCREMNT"){ return state+1}
-        if(action.type === "DECREMNT") {return state-1}
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          ...state,
+          count: state.count + 1,
+        };
+
+      case "DECREMENT":
+        return { ...state, count: state.count - 1 };
+
+      case "RESET":
+        return { ...state, count: 0 };
+
+      default:
+        return state;
     }
+  };
 
-    //const [count, setCount] = useState(0)
-    const [count, dispatch] = useReducer(reducer, 0)
-    //console.log(useReducer(reducer, 0));
-    
-
-    return (
-        <>
-            <div>
-                <h1>{count}</h1>
-                <button onClick={() => dispatch({type:"INCREMENT"})} >Increment</button><br /><br />
-                <button onClick={() => dispatch({type:"DECREMENT"})} >Decrement</button>
-            </div>
-        </>
-    )
-}
+  //   const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div >
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+    </div>
+  );
+};
