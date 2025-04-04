f702cd8a0631faec7e5d9b0771c7a7a3a2c480f6
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-04 17:59:16 +0530

    memo hook

--- react_19/src/App.jsx
+++ react_19/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-import { BioProvider } from './components/hooks/ContextAPI'
-import { Home } from './components/hooks/ContextAPI/home'
-import { About } from './components/hooks/ContextAPI/about'
-import { DarkLight, ThemeProvider } from './components/hooks/ContextAPI/DarkLight'
-import { ReducerComp } from './components/hooks/UseReducer'
 
+import MemoParentComponent from './components/hooks/Memo/UseMemo'
 
 
 const  App = () => {
   return (
     <>
-      <ReducerComp />
+      <MemoParentComponent />
     </>
   )
 
